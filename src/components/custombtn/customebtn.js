dea565c54bdce48cdf94c3ff87f2f726d57f6649
mehranali717 <Mehranalee110@gmail.com>
2023-11-03 03:29:36 +0500

    debugger removed

--- src/components/custombtn/customebtn.js
+++ src/components/custombtn/customebtn.js
@@ -1,5 +1,4 @@
 const Custombtn =(props)=> {
-    debugger
     return <button {...props} className={`text-[13px] text-[#fff] font-[600] rounded-full py-[3px] px-[12px] capitalize ${props.className}`} >{props.label}</button>
 }
 export default Custombtn;
