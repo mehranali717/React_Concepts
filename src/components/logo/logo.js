e74ce8844ef3986ccec3ad04711103db0f4d9a86
mehranali717 <Mehranalee110@gmail.com>
2023-11-03 23:24:08 +0500

    Anchor replaced by Link

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,6 +1,7 @@
 import Image from "../image/image";
 import LogoImage from "../../assets/images/logo.png"
+import { Link } from "react-router-dom";
 const Logo =()=> {
-    return <a href="/"><Image image={LogoImage}/></a>
+    return <Link to="home"><Image image={LogoImage}/></Link>
 }
 export default Logo ;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import useFavorites from "../../hooks/favoriteHook/useFavorite";
 
 const Navbar =()=> {
     const [favorites, setAddRemoveFavorites] = useFavorites("favorites");
     return <nav>
                 <ul>
-                    <li><a href="favorites" className="text-[#fff] text-[16px] capitalize font-[500] relative">favorites <span className="circularBadge">{favorites.length}</span></a></li>
+                    <li><Link to="favorites" className="text-[#fff] text-[16px] capitalize font-[500] relative">favorites <span className="circularBadge">{favorites.length}</span></Link></li>
                 </ul>
     </nav>
 }
